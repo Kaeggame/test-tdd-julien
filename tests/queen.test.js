bb5a4d7780d3c71104e99c91430e822172a1b499
Flamby <66632621+Kaeggame@users.noreply.github.com>
2025-02-12 14:46:11 +0100

    Test add N2 & N3 queens

--- tests/queen.test.js
+++ tests/queen.test.js
@@ -3,3 +3,11 @@ const solveNQueens = require("../index");
 test("N=1 [['Nothing']]", () => {
     expect(solveNQueens(1)).toEqual([["Nothing"]]);
 });
+
+test("N=2 [['Nothing']]", () => {
+    expect(solveNQueens(2)).toEqual([["Nothing"]]);
+});
+
+test("N=3 [['Nothing']]", () => {
+    expect(solveNQueens(3)).toEqual(["Nothing"]);
+});
