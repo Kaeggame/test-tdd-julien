3514729cf711e07bb4e4c3e2a45b38ed8ed20e66
Flamby <66632621+Kaeggame@users.noreply.github.com>
2025-02-12 16:13:15 +0100

    Test number of queens on board but 8

--- tests/queen.test.js
+++ tests/queen.test.js
@@ -27,3 +27,19 @@ test("Queens count 4", () => {
         expect(count).toBe(n);
     });
 });
+
+test("Queens count 8", () => {
+    const n = 8;
+    const solutions = solveNQueens(n);
+    solutions.forEach(board => {
+        let count = 0;
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[i].length; j++) {
+                if (board[i][j] === 'Q') {
+                    count++;
+                }
+            }
+        }
+        expect(count).toBe(n);
+    });
+});
