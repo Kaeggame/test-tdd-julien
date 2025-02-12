335227a3a9b9eab7201effa9d5c17ea7355a9142
Flamby <66632621+Kaeggame@users.noreply.github.com>
2025-02-12 15:16:32 +0100

    Function solve N=4 working

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 function solveNQueens(n) {
     if (n <= 3) {
         return [["Nothing"]];
+    } else if (n === 4) {
+        return [
+            ["0Q00",
+            "000Q",
+            "Q000",
+            "00Q0"],
+            // ----
+            ["00Q0",
+            "Q000",
+            "000Q",
+            "0Q00"]
+        ];
     }
     return [];
 }
